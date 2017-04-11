0ab11688dd0b38e464d1521420468dee18320c73
James Keats <james.keats@mymail.champlain.edu>
2017-04-11 00:44:24 -0400

    Cleanup the autoconvert script.

--- autoconvert.py
+++ autoconvert.py
@@ -1,35 +1,28 @@
-# Thanks to:
+# Some help from an outdated post at:
 # http://stackoverflow.com/a/31624001
 
-import sys
 import os
 import time
-import glob
 import win32com.client
 
 FILENAME = "Understanding Git"
 
 def main():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    success = PPTtoPDF(dir_path + "\\" + FILENAME + ".pptx", dir_path + "\\" + FILENAME + ".pdf")
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
+    success = convertPPTtoPDF(dir_path + FILENAME + ".pptx", dir_path + FILENAME + ".pdf")
 
-    if success:
-        print("Success!")
-    else:
-        print("Could not copy file.")
-    
+    return success
 
-def PPTtoPDF(inputFileName, outputFileName, formatType = 32):
+def convertPPTtoPDF(inputFileName, outputFileName):
     try:
-        if outputFileName[-3:] != 'pdf':
-            outputFileName = outputFileName + ".pdf"
-
         powerpoint = win32com.client.Dispatch("Powerpoint.Application")
-        powerpoint.Visible = 1
         
+        # call open, then sleep to give powerpoint the time to process the command
         deck = powerpoint.Presentations.Open(inputFileName)
         time.sleep(0.5)
-        deck.SaveAs(outputFileName, formatType) # formatType = 32 for ppt to pdf
+
+        # save the file as the pdf. formatType is 32 for pdf (according to MS docs)
+        deck.SaveAs(outputFileName, 32)
         deck.Close()
         
         powerpoint.Quit()
